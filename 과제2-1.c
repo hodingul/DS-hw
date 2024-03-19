99c276ffed68142ad0e2b7145183c4d23a763877
hodingul <127499708+hodingul@users.noreply.github.com>
2024-03-19 15:37:50 +0900

    Add files via upload

---
+++ 과제2-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+	int array[2][3][4];
+	int i, j, k, value = 1;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			for (k = 0; k < 4; k++)
+			{
+				array[i][j][k] = value;
+				printf("array[%d][%d][%d] = %d \n", i, j, k, array[i][j][k]);
+				value++;
+			}
+		}
+	}
+	getchar();
+	return 0;
+}
